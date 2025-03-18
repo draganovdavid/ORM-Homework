c6c629349dc9ab42dd13c3015640b5aab7e7c807
David Draganov <daviddraganov891@gmail.com>
2025-03-18 09:27:25 +0200

    New method DeleteProduct added

---
+++ ORM-Homework-main/ORM-Homework-main/Warehouse/Warehouse/Data/DbConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Warehouse.Data
+{
+    public static class DbConfiguration
+    {
+        public const string ConnectionString = "Server=DAVID\\SQLEXPRESS;Database=Warehouse;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
